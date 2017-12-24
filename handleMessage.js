54faac743e77ebdab181cdc317357aaac17e5635
Mario Marroquin <mmarroking@gmail.com>
2017-12-24 09:08:03 -0800

    Handle edge case of user typing in response instead of choosing an option.

--- handleMessage.js
+++ handleMessage.js
@@ -3,24 +3,34 @@ const state = require('./state');
 
 // Handles messages events
 module.exports = function handleMessage(sender_psid, received_message) {
-  var nextState = received_message.quick_reply.payload;
-  if (state[nextState].options.length !== 0) {
-    var quickreplies = state[nextState].options.map(function(options) {
-      return {
-        content_type: 'text',
-        title: options[0],
-        payload: options[1]
-      };
-    });
-    callSendAPI(sender_psid, {
-      text: state[nextState].text,
-      quick_replies: quickreplies
-    });
+  if (received_message.quick_reply) {
+    var nextState = received_message.quick_reply.payload;
+    if (state[nextState].options.length !== 0) {
+      var quickreplies = state[nextState].options.map(function(options) {
+        return {
+          content_type: 'text',
+          title: options[0],
+          payload: options[1]
+        };
+      });
+      callSendAPI(sender_psid, {
+        text: state[nextState].text,
+        quick_replies: quickreplies
+      });
+    } else {
+      callSendAPI(sender_psid, {
+        text: state[nextState].text
+      });
+      var newState = { quick_reply: { payload: state[nextState].nextState } };
+      setTimeout(function() {
+        handleMessage(sender_psid, newState);
+      }, 2000);
+    }
   } else {
     callSendAPI(sender_psid, {
-      text: state[nextState].text
+      text: 'Please reply by clicking on an option.'
     });
-    var newState = { quick_reply: { payload: state[nextState].nextState } };
+    var newState = { quick_reply: { payload: '1' } };
     setTimeout(function() {
       handleMessage(sender_psid, newState);
     }, 2000);
