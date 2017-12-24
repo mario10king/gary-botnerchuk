525336bd27c4adb6097a9b93b88607968e05e9b3
Mario Marroquin <mmarroking@gmail.com>
2017-12-23 23:35:11 -0800

    Get Started functionality

--- handlePostback.js
+++ handlePostback.js
@@ -1,2 +1,19 @@
+const sendMessage = require("./sendMessage").callSendAPI
 // Handles messaging_postbacks events
-module.exports = function (sender_psid, received_postback) {}
+module.exports = function (sender_psid, received_postback) {
+  sendMessage(sender_psid, {
+    "text": "Hey this is Gary. Do you need advice.",
+    "quick_replies":[
+      {
+        "content_type":"text",
+        "title":"Yes",
+        "payload":"3",//state step it needs to advance too
+      },
+      {
+        "content_type":"text",
+        "title":"No",
+        "payload":"2"
+      }
+    ]
+  });
+}
