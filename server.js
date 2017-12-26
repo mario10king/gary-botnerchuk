19a949253cb44808d245ddeb27f2ef8076b371b1
Mario Marroquin <mmarroking@gmail.com>
2017-12-26 12:07:42 -0800

    Rename file to better show its function

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   app = express().use(bodyParser.json()), // creates express http server
   getRequest = require('./webhookVerify.js'),
-  postRequest = require('./index.js');
+  postRequest = require('./router.js');
 
 // Sets server port and logs message on success
 app.listen(process.env.PORT || 1337, () => console.log('webhook is listening'));
